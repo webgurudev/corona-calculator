e52de1bc00ee7bfb81c44b2da35a5bb13e1578ff
Archy de Berler <archy.deberker@gmail.com>
2020-03-17 22:18:08 -0400

    Add TODOs and data for global age distribution

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 _BED_DATA_PATH = Path(__file__).parent / "world_bank_bed_data.csv"
 _MORTALITY_DATA_PATH = Path(__file__).parent / "mortality_and_hospitalization_by_age.csv"
-
+_GLOBAL_AGE_DISTRIBUTION_DATA_PATH = Path(__file__).parent / '/PopulationAgeSex-20200317101235.xlsx'
 
 def _get_latest_bed_estimate(row):
     non_empty_estimates = [float(x) for x in row.values if float(x) > 0]
@@ -75,5 +75,19 @@ def process_mortality_by_demographics(path=_MORTALITY_DATA_PATH):
 
     return df
 
-if __name__ == '__main__':
-    process_mortality_by_demographics()
+
+def global_demographics_by_age(path=_GLOBAL_AGE_DISTRIBUTION_DATA_PATH):
+    """
+    Demographics by age for the whole world, from https://population.un.org/wpp/DataQuery/.
+    We could use the tool to obtain country-specific demographic information but that would involve
+    lots of data normalization and adding a large data file, so let's approximate with global
+    demographics for now.
+    """
+    # TODO read in the data
+    # TODO normalize to the same ranges as our mortality data
+    pass
+
+
+def apply_demographic_distribution_to_mortality_data(df_mortality, df_demographics):
+    # TODO combine the two to obtain absolute mortality and hospitalization numbers
+    pass
